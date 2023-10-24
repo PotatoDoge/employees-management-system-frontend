faa9faf027da17d1c9718379a64a1d4ed1eafe7e
bprado <bastian.prado.v@gmail.com>
2023-10-23 22:04:35 -0700

    Add sample header and assets

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getHeadClass(): string {
+    let styleClass = '';
+    if (this.collapsed && this.screenWidth > 768) {
+      styleClass = 'head-trimmed';
+    } else {
+      styleClass = 'head-md-screen';
+    }
+    return styleClass;
   }
-
 }
