550cd04f8afe17281a639f7e8c333844b16ec2c7
bprado <bastian.prado.v@gmail.com>
2023-10-27 21:52:44 -0700

    Add simple card responsiveness

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog/public-api';
+import { Employee, EmployeeService } from '../employee-modal-service.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  example = ['A', 'b', 'c'];
+  dataSource = new MatTableDataSource<Employee>([]);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private employeeService: EmployeeService) {}
+  ngOnInit() {
+    this.employeeService.getEmployeesFromComponent().subscribe((employees) => {
+      this.dataSource.data = employees;
+    });
   }
-
 }
