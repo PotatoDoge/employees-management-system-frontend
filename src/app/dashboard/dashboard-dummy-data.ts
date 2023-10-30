b9826e6784b74da60f8276c7edb259042b190af3
bprado <bastian.prado.v@gmail.com>
2023-10-30 15:07:58 -0700

    Add testing images and responsiveness

---
+++ src/app/dashboard/dashboard-dummy-data.ts
@@ -0,0 +1,14 @@
+export const house_images = [
+  {
+    url: '../../assets/houses/casa.jpeg',
+  },
+  {
+    url: '../../assets/houses/casa2.jpeg',
+  },
+  {
+    url: '../../assets/houses/casa3.jpeg',
+  },
+  {
+    url: '../../assets/houses/casa4.jpg',
+  },
+];
